eaae1f4949574c0777e1eb72d65d2404a8750bb4
huevo42 <gregsmith42@gmail.com>
2024-08-06 20:38:20 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -22,3 +22,12 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_security_group_rule" "blog_everything_out" {
+  type        = "egress"
+  from_port   = 0
+  to_port     = 0
+  protocol    = "-1"
+  cidr_blocks = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.blog.id
+}
